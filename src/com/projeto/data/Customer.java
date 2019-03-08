cab9dc75c7058cea0ab89c02436fe1fbd3fa649c
Guilherme <guiborgesTi>
2019-03-07 23:26:37 -0300

    first commit

---
+++ src/com/projeto/data/Customer.java
@@ -0,0 +1,19 @@
+package com.projeto.data;
+
+public class Customer {
+	
+	private String name;
+	
+	public Customer(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	@Override
+	public String toString() {
+		return this.name;
+	}
+}
